93dc569cee9ec533b3f3369c1bbcd2f28b45a523
akhilagrawal14 <akhilagrawal14@gmail.com>
2022-11-09 23:43:55 +0530

    Adding image_info first version

---
+++ README.md
@@ -0,0 +1,3 @@
+"# computer_vision" 
+
+This Project is just to bush up by computer vision skills and to make a as complete as it could be for all computer vsiion algorithms and work.
---
+++ image_info.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import os
+
+def get_image_info(image):
+    """INPUT:
+        image: cv2 image
+        OUTPUT:
+        dict containing info
+    """
+    image_size=image.shape
+    size=len(image_size)
+    info={}
+    image_type={2:"GRAYSCALE",3:"COLOR",4:"RGBA"}
+    info['height']=image_size[0]
+    info['width']=image_size[1]
+    
+    if size>=2 and image_size[2]!=1:
+        info['channel']=image_size[2]
+    if size>=3:
+        info['transperancy']=image_size[3]
+    info['type']=image_type[size]
+    print(info)
+    return info
+    
+def isgray(imgpath):
+    img = cv2.imread(imgpath)
+    if len(img.shape) < 3: return True
+    if img.shape[2]  == 1: return True
+    b,g,r = img[:,:,0], img[:,:,1], img[:,:,2]
+    if (b==g).all() and (b==r).all(): return True
+    print("given image is not a grayscale image")
+    return False
+
+def read_image_from_path(image_path,required_format="RGB"):
+    
+    if required_format == "RGB":
+        img = cv2.imread(image_path,cv2.IMREAD_COLOR) #no transparency channel
+        img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    elif required_format == "BGR":
+        img = cv2.imread(image_path,cv2.IMREAD_COLOR) #no transparency channel
+    elif required_format == "GRAYSCALE":
+        img = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+    elif required_format == "UNCHANGE":
+        img = cv2.imread(image_path,cv2.IMREAD_UNCHANGED) #good if transperency or alpha
+    else:
+        img=np.zeros((256,256,3), np.uint8) #no image so giving black image
+    
+    return img
+
+def image_write(image,file_name,current_format="RGB",extension=".jpeg",path_to_save="./images"):
+    full_path=os.path.join(path_to_save,file_name+extension)
+    try:
+        if current_format=="RGB":
+            image=cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        response=cv2.imwrite(full_path,image)
+    except Exception as e:
+        print(f"Error occured while writing into {path_to_save}: {e}")
+    return response
+
+if "__name__"=="__main__":
+    pass
