aafc38ee0fb2618f861695f3a29cc19343b51b82
akhilagrawal14 <akhilagrawal14@gmail.com>
2022-11-11 01:58:57 +0530

    fixed function bug

--- image_info.py
+++ image_info.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import os
 
+import utils
+
 def get_image_info(image):
     """INPUT:
         image: cv2 image
@@ -9,18 +11,20 @@ def get_image_info(image):
         dict containing info
     """
     image_size=image.shape
+    #print(image_size)
     size=len(image_size)
+  
+    print(f"new size {size} , image_size: {image_size}")
     info={}
     image_type={2:"GRAYSCALE",3:"COLOR",4:"RGBA"}
     info['height']=image_size[0]
     info['width']=image_size[1]
     
-    if size>=2 and image_size[2]!=1:
+    if size>2 :
         info['channel']=image_size[2]
-    if size>=3:
+    if size>3:
         info['transperancy']=image_size[3]
     info['type']=image_type[size]
-    print(info)
     return info
     
 def isgray(imgpath):
@@ -33,6 +37,11 @@ def isgray(imgpath):
     return False
 
 def read_image_from_path(image_path,required_format="RGB"):
+    
+    print(f"given image path : ->{image_path}")
+    
+    if isgray(image_path):
+        required_format="GRAYSCALE"
     
     if required_format == "RGB":
         img = cv2.imread(image_path,cv2.IMREAD_COLOR) #no transparency channel
@@ -50,6 +59,7 @@ def read_image_from_path(image_path,required_format="RGB"):
 
 def image_write(image,file_name,current_format="RGB",extension=".jpeg",path_to_save="./images"):
     full_path=os.path.join(path_to_save,file_name+extension)
+    utils.create_dir(path_to_save)
     try:
         if current_format=="RGB":
             image=cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -58,5 +68,19 @@ def image_write(image,file_name,current_format="RGB",extension=".jpeg",path_to_s
         print(f"Error occured while writing into {path_to_save}: {e}")
     return response
 
-if "__name__"=="__main__":
-    pass
+if __name__ == "__main__":
+    
+    image_path=r"C:\Users\akhil\OneDrive\Pictures\me\IMG_20221010_112517.jpg"
+    
+    image_rgb=read_image_from_path(image_path,required_format="RGB")
+    print(f"Is image grayscale: {isgray(image_path)}")
+    info_rgb=get_image_info(image_rgb)
+    print(f"image info:{info_rgb}")
+    
+    
+    image_path2=r"C:\Users\akhil\OneDrive\Pictures\me\vaccine_qr_code_akhil.jpg"
+    image_grey=read_image_from_path(image_path2,required_format="RGB")
+    print(f"Is image grayscale: {isgray(image_path2)}")
+    info_rgb2=get_image_info(image_grey)
+    print(f"image info:{info_rgb2}")
+    
---
+++ utils.py
@@ -0,0 +1,10 @@
+import os
+
+
+
+def create_dir(path):
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print(f"ERROR: creating directory with name {path}")
